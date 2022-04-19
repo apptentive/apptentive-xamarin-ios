38e3382e202827100c75460e86543b901e5715a5
luqmaan <luq.j.khan@gmail.com>
2022-04-19 12:37:53 -0500

    integrate new binding with sample app

---
+++ ApptentiveKit.iOS/ApptentiveKit.iOS/obj/Debug/iOS/ApptentiveKit.iOS/TermsAndConditions.g.cs
@@ -0,0 +1,70 @@
+//
+// Auto-generated from generator.cs, do not edit
+//
+// We keep references to objects, so warning 414 is expected
+#pragma warning disable 414
+using System;
+using System.Drawing;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics.CodeAnalysis;
+using UIKit;
+using GLKit;
+using Metal;
+using CoreML;
+using MapKit;
+using Photos;
+using ModelIO;
+using Network;
+using SceneKit;
+using Contacts;
+using Security;
+using Messages;
+using AudioUnit;
+using CoreVideo;
+using CoreMedia;
+using QuickLook;
+using CoreImage;
+using SpriteKit;
+using Foundation;
+using CoreMotion;
+using ObjCRuntime;
+using AddressBook;
+using MediaPlayer;
+using GameplayKit;
+using CoreGraphics;
+using CoreLocation;
+using AVFoundation;
+using NewsstandKit;
+using FileProvider;
+using CoreAnimation;
+using CoreFoundation;
+using NetworkExtension;
+#nullable enable
+#if !NET
+using NativeHandle = System.IntPtr;
+#endif
+namespace ApptentiveKit.iOS {
+	[Register("_TtC13ApptentiveKit18TermsAndConditions", true)]
+	public unsafe partial class TermsAndConditions : NSObject {
+		[BindingImpl (BindingImplOptions.GeneratedCode | BindingImplOptions.Optimizable)]
+		static readonly IntPtr class_ptr = Class.GetHandle ("_TtC13ApptentiveKit18TermsAndConditions");
+		public override IntPtr ClassHandle { get { return class_ptr; } }
+		[BindingImpl (BindingImplOptions.GeneratedCode | BindingImplOptions.Optimizable)]
+		[EditorBrowsable (EditorBrowsableState.Advanced)]
+		protected TermsAndConditions (NSObjectFlag t) : base (t)
+		{
+			IsDirectBinding = GetType ().Assembly == global::ApiDefinitions.Messaging.this_assembly;
+		}
+
+		[BindingImpl (BindingImplOptions.GeneratedCode | BindingImplOptions.Optimizable)]
+		[EditorBrowsable (EditorBrowsableState.Advanced)]
+		protected internal TermsAndConditions (IntPtr handle) : base (handle)
+		{
+			IsDirectBinding = GetType ().Assembly == global::ApiDefinitions.Messaging.this_assembly;
+		}
+
+	} /* class TermsAndConditions */
+}
